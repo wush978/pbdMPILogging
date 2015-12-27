026c27d150ef2407a208c6847a9b9a9a8302ec55
Wush Wu <wush978@gmail.com>
2015-12-27 16:06:30 +0800

    test new feature: append original formatter

--- tests/testthat/test-convert_to_parallel_logger.R
+++ tests/testthat/test-convert_to_parallel_logger.R
@@ -7,3 +7,15 @@ test_that("The logger is successfully converted", {
   out2 <- capture.output(loginfo("test"))
   expect_equal(sprintf("(0/1) %s", out1), out2)
 })
+
+test_that("The logger is successfully converted even if it is customized", {
+  basicConfig()
+  handler <- getHandler("basic.stdout", getLogger())
+  handler$formatter <- function(record) {
+    paste(record$timestamp, record$msg, sep = ":")
+  }
+  out1 <- capture.output(loginfo("test"))
+  convert_to_parallel_logger(getLogger())
+  out2 <- capture.output(loginfo("test"))
+  expect_equal(sprintf("(0/1) %s", out1), out2)
+})
