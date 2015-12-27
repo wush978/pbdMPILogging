2e6d9a103293b9f28c339a52e91a0502092f414f
Wush Wu <wush978@gmail.com>
2015-12-27 16:06:13 +0800

    init unit test

---
+++ tests/testthat/test-convert_to_parallel_logger.R
@@ -0,0 +1,9 @@
+context("Testing pbdMPILogging::convert_to_parallel_logger")
+
+test_that("The logger is successfully converted", {
+  basicConfig()
+  out1 <- capture.output(loginfo("test"))
+  convert_to_parallel_logger(getLogger())
+  out2 <- capture.output(loginfo("test"))
+  expect_equal(sprintf("(0/1) %s", out1), out2)
+})
